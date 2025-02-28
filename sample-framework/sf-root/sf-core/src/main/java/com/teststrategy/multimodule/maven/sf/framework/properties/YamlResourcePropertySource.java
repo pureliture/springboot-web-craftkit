800815f5db48bd7d0bba2b45c84b85b32bf1c3fa
lsh1756 <tkdgur1756@naver.com>
2025-02-28 21:01:25 +0900

    [sf-core] ✨ feat: @PropertySource 에서 yaml 사용하기 위한 설정 추가

---
+++ sample-framework/sf-root/sf-core/src/main/java/com/teststrategy/multimodule/maven/sf/framework/properties/YamlResourcePropertySource.java
@@ -0,0 +1,14 @@
+package com.teststrategy.multimodule.maven.sf.framework.properties;
+
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.IOException;
+
+
+public class YamlResourcePropertySource extends PropertiesPropertySource {
+
+    public YamlResourcePropertySource(String name, EncodedResource resource) throws IOException {
+        super(name, new YamlPropertiesProcessor(resource.getResource()).createProperties());
+    }
+}
