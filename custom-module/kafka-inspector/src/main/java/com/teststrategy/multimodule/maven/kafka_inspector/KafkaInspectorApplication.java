d899bea7ce397d7e8ba70b0763a58afa32b06fda
lsh1756 <lsh1756@lguplus.co.kr>
2025-11-04 17:01:34 +0900

    :wrench: compose maven multi module structure
    (cherry picked from commit f39d00dea8c6ce8c85a168ae1149606c1ed20bc5)

---
+++ custom-module/kafka-inspector/src/main/java/com/teststrategy/multimodule/maven/kafka_inspector/KafkaInspectorApplication.java
@@ -0,0 +1,13 @@
+package com.teststrategy.multimodule.maven.kafka_inspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaInspectorApplication.class, args);
+	}
+
+}
---
+++ custom-module/kafka-inspector/src/test/java/com/teststrategy/multimodule/maven/kafka_inspector/KafkaInspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teststrategy.multimodule.maven.kafka_inspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaInspectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
